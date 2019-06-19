0ccc1902e9a8aa666432f9cf37a657da8c979605
上傳人名字 <auroral.13king518@gmail.com>
2019-06-19 16:11:19 +0800

    Create GoogleMap.js

---
+++ src/GoogleMap/components/GoogleMap.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import GoogleMapReact from 'google-map-react';
+
+const Wrapper = styled.main`
+  width: 100%;
+  height: 100%;
+  position:absolute;
+`;
+
+const GoogleMap = ({ children, ...props }) => (
+  <Wrapper>
+    <GoogleMapReact
+      bootstrapURLKeys={{
+        key: process.env.REACT_APP_MAP_KEY,
+      }}
+      {...props}
+    >
+      {children}
+    </GoogleMapReact>
+  </Wrapper>
+);
+
+GoogleMap.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+};
+
+GoogleMap.defaultProps = {
+  children: null,
+};
+
+export default GoogleMap;
