c0abb32b84c9c7a6f94b90fd3ae259d61be8d91c
YunHisangTang <r07631005@ntu.edu.tw>
2019-06-29 20:10:18 +0800

    TravMap delete

--- src/GoogleMap/components/SideBar.js
+++ src/GoogleMap/components/SideBar.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarker } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faMapMarker } from '@fortawesome/free-solid-svg-icons';
 const SideBar = ({ places, userAddclear }) => (
-  <ul className="sidebar-list">
+  <ul className='sidebar-list'>
     {places.length > 0 &&
       places.map((place, index) => (
-        <li className="sidebar-list__item" key={place.place_id}>
+        <li className='sidebar-list__item' key={place.place_id}>
           {/* <div style={{ position: "absolute", top: "10px", right: "10px" }}>{index + 1}</div> */}
 
-          <FontAwesomeIcon icon={faMapMarker} className="sidebar-list__icon" />
-          <div className="sidebar-list__num" onClick={index => userAddclear(index)}>
-            {index + 1}
+          <div className='sidebar-list__num' onClick={index => userAddclear(index)}>
+            <span className='sidebar-list__span'>{index + 1}</span>
           </div>
-          <h3 className="sidebar-list__header">
-            <span className="sidebar-list__img" style={{ backgroundImage: `url(${place.icon})` }} />
+          <FontAwesomeIcon icon={faTimes} className='sidebar-list__spanicon' />
+
+          <FontAwesomeIcon icon={faMapMarker} className='sidebar-list__icon' />
+
+          <h3 className='sidebar-list__header'>
+            <span className='sidebar-list__img' style={{ backgroundImage: `url(${place.icon})` }} />
             {place.name}
           </h3>
-          <p className="sidebar-list__addr"> {place.formatted_address} </p>
+          <p className='sidebar-list__addr'> {place.formatted_address} </p>
         </li>
       ))}
   </ul>
